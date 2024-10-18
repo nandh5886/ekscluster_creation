bb04ab9b589c908f2e06a31298fa4f9363ba1e8c
nandh5886 <115916691+nandh5886@users.noreply.github.com>
2024-10-18 14:26:50 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,14 @@
+locals {
+  region = "us-east-1"
+  cluster_name = "eksdemo-cluster"
+  vpc = "10.0.0.0/16"
+  azs = ["us-east-1a," "us-east-1b"]
+  public_subnets = ["10.0.0.1/24", "10.0.0.2/24"]
+  private_subnets = ["10.0.0.3/24", "10.0.0.4/24"]
+  intra_subnets = ["10.0.0.5/24", "10.0.0.6/24"]
+
+}
+provider "aws" {
+    region = "us-east-1"
+  
+}
