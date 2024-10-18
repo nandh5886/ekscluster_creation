44fcde94280bef205892f240db0d9d02b0d898f3
nandh5886 <115916691+nandh5886@users.noreply.github.com>
2024-10-18 14:28:00 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "my-cluster"
+  cluster_name    = "eksdemo-cluster"
   cluster_endpoint_public_access  = true
 
   cluster_addons = {
